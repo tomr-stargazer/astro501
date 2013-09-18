eb3938bd545e57525647837b2f65abf3ccdec265
Tom Rice <t.rice90@gmail.com>
2013-09-17 20:07:27 -0400

    Wrote the plotting function for 2b.

--- hw1a/problem_2.py
+++ hw1a/problem_2.py
@@ -21,4 +21,25 @@ def problem_2b():
     # n(lambda) for Schott F2
     n2 = lambda lam: 1.6706 - 0.0862*lam
 
+    lam_range = np.arange(0.4, 0.85, 0.05)
+
+    fig = plt.figure()
+
+    plt.plot( lam_range, n2(lam_range), lw=2, label="Schott F2")
+    plt.plot( lam_range, n1(lam_range), lw=2, label="BK7")
+
+    plt.legend()
+
+    plt.ylim(1.45, 1.7)
+    plt.ylabel(r"Index of refraction $n(\lambda)$")
+    plt.xlabel(r"Wavelength $\lambda (\mu m)$")
+
+    plt.title("Problem 2b")
+
+    plt.text(0.5, 1.63, r"$n_2(\lambda) = 1.6707 - 0.0862 \lambda/\mu m$", 
+             fontsize=18)
+    plt.text(0.5, 1.52, r"$n_1(\lambda) = 1.5414 - 0.0418 \lambda/\mu m$", 
+             fontsize=18)
     
+    plt.show()
+    return fig
